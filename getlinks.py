f47cc00e81ec34364a040cadbdce6aae299fa6d6
Your Name <gorokhov@gmx.com>
2019-01-15 21:15:13 +0300

    15.01.2019

---
+++ getlinks.py
@@ -0,0 +1,47 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import time
+import re
+def getsite(url):
+    #getting the url of website from page which find in def main()
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page=urlopen(req).read()
+    soup = BeautifulSoup(page,"html.parser")
+    link = soup.find("a", class_="visit-website").get("href")
+    getmail(link)
+
+def getmail(sitelink):
+    req = Request(sitelink, headers={'User-Agent': 'Mozilla/62.0'})
+    time.sleep(2)
+    page=urlopen(req).read()
+    time.sleep(2)
+    soup = BeautifulSoup(page,"html.parser")
+    time.sleep(1)
+    string = str(page)
+    match=(re.findall(r'[\w.-]+@[\w.-]+', string))
+    i=0
+    while i < len(match):
+        print (match[i])
+        with open('/home/den/mail/base', 'a') as f:
+            f.write(match[i]+'\n')
+        i=i+1
+    with open('/home/den/mail/base', 'a') as f:
+            f.write('--------\n')
+
+def main():
+    req = Request('https://tokenmarket.net/ico-calendar/upcoming', headers={'User-Agent': 'Mozilla/5.0'})
+    page=urlopen(req).read()
+    soup = BeautifulSoup(page,"html.parser")
+    links = []
+    for link in soup.find_all('a', class_="logo-link"):
+        links.append(link.get('href'))
+    #link = soup.find("div", {"class":"div.col-xs-12.col-sm-2.col-lg-3.follow-controls.text-center"}).find("a").get("href")
+    #link = soup.find("a", class_="visit-website").get("href")
+    #for link in soup.find_all('a', attrs={'href': re.compile("^https://")}):
+    i=0
+    while i < len(links):
+    #    print (links[i])
+        getsite(links[i])
+        i=i+1
+
+main()
